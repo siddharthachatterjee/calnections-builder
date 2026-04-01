af6322f44c1dc0ec89f10048431e7ed00695d176
siddharthachatterjee <ninja.siddhartha@gmail.com>
2026-03-31 23:02:34 -0700

    built page

---
+++ src/Connections.js
@@ -0,0 +1,12 @@
+export default function Connections() {
+    return (
+        <>
+        <div className="container">
+            <div className="hero">
+                <h1> Cal-Nections Builder </h1>
+            </div>
+            <main></main>
+        </div>
+        </>
+    )
+}
